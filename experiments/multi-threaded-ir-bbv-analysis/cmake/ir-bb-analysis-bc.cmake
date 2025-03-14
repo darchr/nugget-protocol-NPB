90dafd04a827b04e097854200674823e7b9251aa
studyztp <studyztp@gmail.com>
2025-03-14 12:29:17 -0700

    ready for nugget experiments

---
+++ experiments/multi-threaded-ir-bbv-analysis/cmake/ir-bb-analysis-bc.cmake
@@ -0,0 +1,10 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
+include(base_config)
+
+set(TARGET_NAME "ir_bb_analysis_bc")
+set(REGION_LENGTH 100000000)
+set(HOOK_TARGET "openmp-ir-bb-analysis")
+
+set(OPT_CMD "-O3")
+
+list(APPEND EXTRA_FLAGS "-cpp")
---
+++ experiments/multi-threaded-ir-bbv-analysis/cmake/ir-bb-analysis-exe.cmake
@@ -0,0 +1,24 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
+include(base_config)
+list(APPEND CMAKE_MODULE_PATH "${NUGGET_LIBRARY_PATH}")
+include(Nugget)
+
+set(TARGET_NAME "ir_bb_analysis_exe")
+
+set(LLC_EXTRATION_FILE_PATH
+    ${NUGGET_UTIL_PATH}/cmake/check-cpu-features/llc-command.txt)
+
+if(LLC_EXTRATION_FILE_PATH AND EXISTS ${LLC_EXTRATION_FILE_PATH})
+    nugget_read_list(LLC_CMD ${LLC_EXTRATION_FILE_PATH})
+else()
+    message(WARNING "LLC command not found in ${LLC_EXTRATION_FILE_PATH}")
+endif()
+
+if(LLC_CMD)
+    list(APPEND LLC_CMD -relocation-model=pic -O3)
+endif()
+
+set(BC_FILE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc")
+set(BC_FILE_BASENAME "ir_bb_analysis_bc")
+
+message(STATUS "LLC_CMD: ${LLC_CMD}")
