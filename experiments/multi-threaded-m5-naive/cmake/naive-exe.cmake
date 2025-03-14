495cae2b15d2e34cb431833370e448ff51eb4d77
studyztp <studyztp@gmail.com>
2025-03-14 16:42:44 -0700

    fix a mistake of mixing the config for m5 and papi

--- experiments/multi-threaded-m5-naive/cmake/naive-exe.cmake
+++ experiments/multi-threaded-m5-naive/cmake/naive-exe.cmake
@@ -3,7 +3,7 @@ include(base_config)
 list(APPEND CMAKE_MODULE_PATH "${NUGGET_LIBRARY_PATH}")
 include(Nugget)
 
-set(TARGET_NAME "papi_naive_exe")
+set(TARGET_NAME "m5_naive_exe")
 
 set(LLC_EXTRATION_FILE_PATH
     ${NUGGET_UTIL_PATH}/cmake/check-cpu-features/llc-command.txt)
@@ -19,6 +19,12 @@ if(LLC_CMD)
 endif()
 
 set(BC_FILE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc")
-set(BC_FILE_BASENAME "papi_naive_bc")
+set(BC_FILE_BASENAME "m5_naive_bc")
 
 message(STATUS "LLC_CMD: ${LLC_CMD}")
+
+set(EXTRA_LIB_PATHS ${M5_PATH})
+set(EXTRA_INCLUDES ${M5_INCLUDE_PATH})
+set(EXTRA_LIBS "-lm5")
+
+
--- experiments/multi-threaded-papi-naive/cmake/naive-bc.cmake
+++ experiments/multi-threaded-papi-naive/cmake/naive-bc.cmake
@@ -1,8 +1,12 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
 include(base_config)
 
-set(TARGET_NAME "m5_naive_bc")
-set(HOOK_TARGET m5-naive)
+set(TARGET_NAME "papi_naive_bc")
+set(HOOK_TARGET papi-naive)
 
 set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
 set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_bc")
+
+set(EXTRA_LIB_PATHS ${PAPI_PATH}/lib)
+set(EXTRA_INCLUDES ${PAPI_PATH}/include)
+set(EXTRA_LIBS "-lpapi")
