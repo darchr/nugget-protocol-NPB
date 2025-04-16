439f32954460ddf03e953b800a0282b8712a127f
studyztp <studyztp@gmail.com>
2025-04-15 17:50:55 -0700

    add m5-naive cmake script

--- experiments/gem5-m5-naive/cmake/naive-exe.cmake
+++ experiments/gem5-m5-naive/cmake/naive-exe.cmake
@@ -3,7 +3,7 @@ include(base_config)
 list(APPEND CMAKE_MODULE_PATH "${NUGGET_LIBRARY_PATH}")
 include(Nugget)
 
-set(TARGET_NAME "papi_naive_exe")
+set(TARGET_NAME "m5_naive_exe")
 
 set(LLC_EXTRATION_FILE_PATH
     ${NUGGET_UTIL_PATH}/cmake/check-cpu-features/llc-command.txt)
@@ -19,11 +19,11 @@ if(LLC_CMD)
 endif()
 
 set(BC_FILE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc")
-set(BC_FILE_BASENAME "papi_naive_bc")
+set(BC_FILE_BASENAME "m5_naive_bc")
 
 message(STATUS "LLC_CMD: ${LLC_CMD}")
 list(APPEND EXTRA_FLAGS -no-pie)
-set(EXTRA_LIB_PATHS -L${PAPI_PATH}/lib)
-set(EXTRA_INCLUDES -I${PAPI_PATH}/include)
-set(EXTRA_LIBS -lpapi -lpfm)
+set(EXTRA_LIB_PATHS -L${M5_PATH})
+set(EXTRA_INCLUDES -I${M5_INCLUDE_PATH})
+set(EXTRA_LIBS -lm5)
 
