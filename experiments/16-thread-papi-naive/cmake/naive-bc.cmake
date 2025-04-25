b45c123c47d701df3b1314f9a5545bcbc24e5535
studyztp <studyztp@gmail.com>
2025-04-25 21:04:23 +0000

    add 16-thread saphir naive data

---
+++ experiments/16-thread-papi-naive/cmake/naive-bc.cmake
@@ -0,0 +1,12 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
+include(base_config)
+
+set(TARGET_NAME "papi_naive_16_thread_bc")
+set(HOOK_TARGET papi-naive)
+
+set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
+set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_1600000000_bc")
+
+set(EXTRA_LIB_PATHS -L${PAPI_PATH}/lib)
+set(EXTRA_INCLUDES -I${PAPI_PATH}/include)
+set(EXTRA_LIBS "-lpapi")
