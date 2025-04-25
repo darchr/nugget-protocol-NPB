a55c9e4e4dcc56f9c5ca42eeaf92766a8f3a39dd
studyztp <studyztp@gmail.com>
2025-04-25 21:04:43 +0000

    add 4 and 8 thread naive data

---
+++ experiments/4-thread-papi-naive/cmake/naive-bc.cmake
@@ -0,0 +1,12 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
+include(base_config)
+
+set(TARGET_NAME "papi_naive_4_thread_bc")
+set(HOOK_TARGET papi-naive)
+
+set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
+set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_400000000_bc")
+
+set(EXTRA_LIB_PATHS -L${PAPI_PATH}/lib)
+set(EXTRA_INCLUDES -I${PAPI_PATH}/include)
+set(EXTRA_LIBS -lpapi -lpfm)
