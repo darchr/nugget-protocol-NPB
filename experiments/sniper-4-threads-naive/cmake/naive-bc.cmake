9c0c283d14d64d01c510e2fc94397daed0e1e7ab
studyztp <studyztp@gmail.com>
2025-04-30 16:12:01 -0700

    try to use forward declear to avoid early linking

--- experiments/sniper-4-threads-naive/cmake/naive-bc.cmake
+++ experiments/sniper-4-threads-naive/cmake/naive-bc.cmake
@@ -7,5 +7,5 @@ set(HOOK_TARGET sniper-naive)
 set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
 set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_400000000_bc")
 
-set(EXTRA_INCLUDES -I${SNIPER_INCLUDE_PATH})
+# set(EXTRA_INCLUDES -I${SNIPER_INCLUDE_PATH})
 
