f8587793f511ca19a993991f251eb4c6e50ec844
studyztp <studyztp@gmail.com>
2025-05-21 22:21:38 -0700

    add papi profiling optipon and cmake script

--- experiments/1-thread-papi-analysis/cmake/papi-analysis-bc.cmake
+++ experiments/1-thread-papi-analysis/cmake/papi-analysis-bc.cmake
@@ -5,6 +5,7 @@ set(TARGET_NAME "papi_analysis_bc")
 set(REGION_LENGTH 100000000)
 set(HOOK_TARGET "single-threaded-papi-analysis")
 
-set(OPT_CMD "-O2")
-
 list(APPEND EXTRA_FLAGS "-cpp")
+
+set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
+set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_bc")
