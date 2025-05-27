b6f5ed090f1d1d9cbb6a4dc85c904a6c419b0619
studyztp <studyztp@gmail.com>
2025-05-27 12:48:10 -0700

    add navie option

---
+++ experiments/1-thread-papi-naive/cmake/naive-bc.cmake
@@ -0,0 +1,14 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
+include(base_config)
+
+set(TARGET_NAME "papi_naive_bc")
+set(HOOK_TARGET papi-naive)
+
+set(SOURCE_BC_FILE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cbuild/llvm-bc)
+set(SOURCE_BC_FILE_BASENAME "ir_bb_analysis_bc")
+
+list(APPEND EXTRA_FLAGS "-cpp")
+
+set(EXTRA_LIB_PATHS -L${PAPI_PATH}/lib)
+set(EXTRA_INCLUDES -I${PAPI_PATH}/include)
+set(EXTRA_LIBS -lpapi -lpfm)
